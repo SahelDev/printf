eeb6c14b4b4ce5a7a051822f49089bcdc196d876
achrafkassimi <achraf.kassimi.1995@gmail.com>
2023-10-17 13:46:06 -0700

    Write a function that prints.

--- main.h
+++ main.h
@@ -7,64 +7,53 @@
 #include <stdlib.h>
 #include <limits.h>
 
-#define PARAMS_INIT_f {0,0,0,0,0}
-#define PARAMS_INIT_p2 {0,0}
-#define PARAMS_INIT_p3 {0,0,0}
+#define UNUSED(x) (void)(x)
+#define NULL_STRING "(null)"
 
 /**
-* struct parameters --> flags { '+'  ' '  '#'  '0'  '-' }
-* @plus_flag: on if plus_falg specified
-* @space_flag: on if space_flag specified
-* @hashtag_flag: on if hashtag_flag specified
-* @zero_flag: on if zero_flag specified
-* @minus_flag: on if minus_flag specified
-* 
-* struct
+* struct flags - struct for { '+'  ' '  '#'  '0'  '-' }
+* @flag_plus: on if plus_falg specified
+* @flag_space: on if space_flag specified
+* @flag_hashtag: on if hashtag_flag specified
+* @flag_zero: on if zero_flag specified
+* @flag_minus: on if minus_flag specified
 */
-typedef struct flags{
-    int flag_plus;
+typedef struct flags
+{
+	int flag_plus;
 	int flag_space;
 	int flag_hashtag;
 	int flag_zero;
-	int flag_minus;	
+	int flag_minus;
 } flags_5;
 
 /**
-* struct parameters --> width and precision
+* struct wid_per - parameters --> width and precision
 * @width: field width specified
 * @precision: field percision specified
 *
 */
-typedef struct wid_per{
-    int width;
+typedef struct wid_per
+{
+	int width;
 	int precision;
 } wid_per_2;
 
 /**
-* struct parameters --> unsigned and length(l, h)
+* struct paramets - parameters --> unsigned and length(l, h)
 *
 * @unsing: flag if unsigned value
-* 
+*
 * @h_modif: on if h_modifier is specified
 * @l_modif: on if l_modifier is specified
 *
 */
-typedef struct paramets{
-	int unsing 			:1;
-    int h_modif 	    :1;
-	int l_modif 	    :1;
+typedef struct paramets
+{
+	int unsing;
+	int h_modif;
+	int l_modif;
 } paramets_3;
-
-/**
-* struct specifier --> struct token
-*
-* @specifier: format token
-* @f: the function associated
-*/
-typedef struct specifier{
-	char *specifier;
-	int (*f)(va_list, flags_5 *, wid_per_2 *, paramets_3 *);
-} specifier_15;
 
 /* init_struct.c module */
 void flags_init(flags_5 *par);
@@ -73,7 +62,7 @@ void paramet_3_init(paramets_3 *par);
 
 /* vr_format.c*/
 int flag_get(char *s, flags_5 *f);
-char *width_get(char *s,  wid_per_2*p2, va_list ch);
+char *width_get(char *s,  wid_per_2 *p2, va_list ch);
 char *precision_get(char *p, wid_per_2 *p2, va_list ch);
 int modif_get(char *s, paramets_3 *p3);
 
@@ -86,6 +75,9 @@ int _puts(char *str);
 /* _printf.c module */
 int _printf(const char *format, ...);
 
-
+/* print_simple.c */
+int print_char(va_list ch, wid_per_2 *p2, flags_5 *f);
+int print_percent(va_list ch, paramets_3 *p3, wid_per_2 *p2, flags_5 *f);
+int print_string(va_list ch, paramets_3 *p3, wid_per_2 *p2, flags_5 *f);
 
 #endif
